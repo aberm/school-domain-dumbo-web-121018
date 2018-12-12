73655f5fab77e4d1b076216daf86f85df4e3fdb9
Menachem Aber <abermendel@gmail.com>
2018-12-12 22:18:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+
+class School
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  ROSTER = {}
+  
+  def roster
+    ROSTER
+  end
+  
+  def add_student(name, grade)
+    if ROSTER[grade]
+      ROSTER[grade] << name
+    else
+      ROSTER[grade] = []
+      ROSTER[grade] << name
+    end
+  end
+  
+  def grade(grd)
+    ROSTER[grd]
+  end
+  
+  def sort
+    ROSTER
+  end
+  
+end
